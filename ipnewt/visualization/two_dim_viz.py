23c668ecf3d4edfe594010a6685ce3599fc328f8
eytanadler <eytana@umich.edu>
2021-12-03 13:48:50 -0500

    Started visualization tools

---
+++ ipnewt/visualization/two_dim_viz.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+@File    :   two_dim_viz.py
+@Time    :   2021/12/3
+@Desc    :   2D visualization tools
+"""
+
+# ==============================================================================
+# Standard Python modules
+# ==============================================================================
+import matplotlib.pyplot as plt
+
+# ==============================================================================
+# External Python modules
+# ==============================================================================
+import niceplots
+
+# ==============================================================================
+# Extension modules
+# ==============================================================================
+
+# Set up niceplots
+dark_mode = True
+niceplots.setRCParams(dark_mode=dark_mode, set_dark_background=dark_mode)
+
+
