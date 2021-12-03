da68bc432860856518b8966f7cb0428001a83b09
lamkina <lamkin@umich.edu>
2021-12-03 13:12:24 -0500

    updated api

--- IPNewt/api.py
+++ IPNewt/api.py
@@ -1,5 +1,5 @@
-from IPNewt.line_search.line_search import LineSearch
+from IPNewt.line_search.line_search import LineSearch, AdaptiveLineSearch
 from IPNewt.linear.linear_system import LULinearSystem
 from IPNewt.model.model import Model
 from IPNewt.newton.newton import NewtonSolver
-import IPNewt.test_problems as test_problems
+from IPNewt.test_problems.powell import Powell
