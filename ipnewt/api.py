09ac5ce9773fef0813a28c1784e76bdcd538160f
eytanadler <eytana@umich.edu>
2021-12-07 15:40:37 -0500

    Added new line search to API

--- ipnewt/api.py
+++ ipnewt/api.py
@@ -1,4 +1,4 @@
-from ipnewt.line_search.line_search import LineSearch, AdaptiveLineSearch
+from ipnewt.line_search.line_search import LineSearch, AdaptiveLineSearch, BoundsEnforceLineSearch
 from ipnewt.linear.linear_system import LULinearSystem
 from ipnewt.model.model import Model
 from ipnewt.newton.newton import NewtonSolver
